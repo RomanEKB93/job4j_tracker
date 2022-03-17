fb0f898c0336fd068fe66242f62a33e4c4e3fd5e
RomanEKB93 <oma170@hotmail.com>
2022-03-17 20:51:29 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String created = pen.getCreated().format(formatter);
         System.out.println(created);
+        System.out.println(pen);
     }
 }
