556cb8ed851b2cebc23f052889c11cf59d704ced
RomanEKB93 <oma170@hotmail.com>
2022-04-19 19:14:53 +0300

    2. Список адресов [#110062 #290610]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,13 +8,13 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile :: getAddress)
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile :: getAddress)
+                .map(Profile::getAddress)
                 .sorted((x, y) -> x.getCity().compareTo(y.getCity()))
                 .distinct()
                 .collect(Collectors.toList());
